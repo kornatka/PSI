bf890ce09603397f2059c7afa608338c0f81fc64
blazej-baranowski <35075848+blazej-baranowski@users.noreply.github.com>
2018-01-13 22:17:31 +0100

    Add files via upload

---
+++ Blazej_Baranowski/db.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get install software-properties-common
+sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
+sudo add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://mariadb.kisiek.net/repo/10.2/ubuntu trusty main'
+sudo apt-get install mariadb-server
+echo 'Instalacja zakończona!'
