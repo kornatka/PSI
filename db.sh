9f71cc036307a2afaa467ba22c27cf6aa7d204fe
mkowalska1 <35264516+mkowalska1@users.noreply.github.com>
2018-01-21 15:26:43 +0100

    Add files via upload

---
+++ db.sh
@@ -0,0 +1,10 @@
+sudo apt update
+sudo apt -y full-upgrade
+
+sudo apt -y install mariadb-server
+
+mysql -u root << SKRYPT
+create database drupal;
+GRANT ALL PRIVILEGES ON *.* TO 'magda'@'*' IDENTIFIED BY 'qwerty' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'drupal'@'192.168.56.2' IDENTIFIED BY 'qwerty1' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'192.168.56.3' IDENTIFIED BY 'qwerty2';
